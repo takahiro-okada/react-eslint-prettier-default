b6aaf9af0aa20768c006f392c9a9462ca19e0fe3
takahiro okada <t.okada@koshikiad.com>
2021-07-13 09:55:49 +0900

    とりあえずESLintとPrettierを入れてみた

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+	return (
+		<div className="App">
       <header className="App-header">
-        <h1>Reactの環境構築をしていきます！！</h1>
-      </header>
-    </div>
-  );
+				<h1>Reactの環境構築をしていきます！！</h1>
+			</header>
+		</div>
+	);
 }
 
 export default App;
