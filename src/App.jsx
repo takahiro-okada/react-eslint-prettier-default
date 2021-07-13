d253c88a9cc62a811edd5cd60684c21ba0fb69a6
takahiro okada <t.okada@koshikiad.com>
2021-07-14 06:04:02 +0900

    一旦ESLintとPrettierの設定を完了

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Reactの環境構築をしていきます！！</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
